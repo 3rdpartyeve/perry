bd6158af33abdbb4d3211482a83817f98d3cc798
Peter Petermann <peter.petermann@gameforge.com>
2014-01-24 14:11:42 +0100

    cleanup

--- src/Perry/Setup.php
+++ src/Perry/Setup.php
@@ -1,7 +1,7 @@
 <?php
 namespace Perry;
 
-class Setup
+final class Setup
 {
     /**
      * @var Setup
@@ -36,5 +36,4 @@ class Setup
     public static $crestUrl = "http://public-crest.eveonline.com";
     public static $thoraUrl = "http://thora.3rdpartyeve.net";
     public static $bindToIp = "0.0.0.0:0";
-
 }
