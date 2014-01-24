d1cea3e598419d32f8effcb959eed97f91081864
Peter Petermann <peter.petermann@gameforge.com>
2014-01-24 14:19:16 +0100

    adding some comments

--- src/Perry/Representation/Eve/v1/TournamentStaticSceneData.php
+++ src/Perry/Representation/Eve/v1/TournamentStaticSceneData.php
@@ -20,18 +20,26 @@ class TournamentStaticSceneData extends Base
      */
     public function setGlobalObjects($globalObjects)
     {
+        // iterating over call global objects to fill them with their references
         foreach ($globalObjects as $object) {
+
             $object->type = new Reference($object->type, "Dear CCP please document this representation");
 
+            // a global object might contain further references
             if (isset($object->planetOrMoonInfo) && !is_null($object->planetOrMoonInfo)) {
+                // reference to 1st heightMap
                 $object->planetOrMoonInfo->heightMap1 = new Reference(
                     $object->planetOrMoonInfo->heightMap1,
                     "Dear CCP please document this representation"
                 );
+
+                // reference to shader Preset
                 $object->planetOrMoonInfo->shaderPreset = new Reference(
                     $object->planetOrMoonInfo->shaderPreset,
                     "Dear CCP please document this representation"
                 );
+
+                // reference to 2nd heightMap
                 $object->planetOrMoonInfo->heightMap2 = new Reference(
                     $object->planetOrMoonInfo->heightMap2,
                     "Dear CCP please document this representation"
