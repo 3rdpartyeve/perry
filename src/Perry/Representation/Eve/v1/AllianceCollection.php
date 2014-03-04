57c9e820e362f9718eacf5a66c86dbe71164668d
Peter Petermann <peter.petermann@gameforge.com>
2014-03-04 16:07:50 +0100

    ported fix

--- src/Perry/Representation/Eve/v1/AllianceCollection.php
+++ src/Perry/Representation/Eve/v1/AllianceCollection.php
@@ -27,7 +27,7 @@ class AllianceCollection extends Base
     public function setItems($items)
     {
         foreach ($items as $item) {
-
+            $item =  new Base($item);
             $item->href = new Reference($item->href, "vnd.ccp.eve.Alliance-v1");
             $this->items[] = $item;
         }
